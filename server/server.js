4b1c0e578514175b9f422e43d78fd4651399952c
root <root@Anonymous.localdomain>
2021-09-25 00:57:10 -0400

    allow cookies

--- server/server.js
+++ server/server.js
@@ -21,6 +21,7 @@ app.use((req, res, next) =>{
     res.setHeader('Access-Control-Allow-Origin', process.env.ORIGIN_ALLOWED)
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
     res.setHeader('Access-Control-Allow-Credentials', true)
+    next()
 })
 
 app.set("trust proxy", true)
